1c9af6182cf288d545cec44ef6661bdaf5bc9e03
Yaroslav Hrabovskyi <yaroslav.hrabovskyi@gmail.com>
2020-08-13 13:09:01 +0300

    Enabled razor runtime compilation.

--- src/Web/WebMVC/Startup.cs
+++ src/Web/WebMVC/Startup.cs
@@ -28,7 +28,8 @@ namespace WebMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
             services.AddHttpClientServices();
             services.AddSession();
             services.Configure<AppSettings>(Configuration);
