ce4ca62d98884a3a98d6006b60af4afcba217df8
Yaroslav Hrabovskyi <yaroslav.hrabovskyi@gmail.com>
2020-08-19 19:17:25 +0300

    Removed Privacy page. Added link to authors catalog.

--- src/Web/WebMVC/Controllers/HomeController.cs
+++ src/Web/WebMVC/Controllers/HomeController.cs
@@ -19,11 +19,6 @@ namespace WebMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
